629b0e3829dd8b5b1d6c444e5897c85d743db8ae
sbkn <sergeybakin87@googlemail.com>
2017-03-03 17:12:14 +0100

    dont mind

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ var WebpackDevServer = require('webpack-dev-server');
 var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
-    publicPath: config.output.publicPath
-  })
-  .listen(3000, '0.0.0.0', function (err, result) {
-    if (err) {
-      console.log(err);
-    }
+	publicPath: config.output.publicPath
+})
+	.listen(3000, '0.0.0.0', function (err, result) {
 
-    console.log('Running at http://0.0.0.0:3000');
-  });
+		if (err) {
+			console.log(err);
+		}
+
+		console.log('Running at http://0.0.0.0:3000');
+	});
